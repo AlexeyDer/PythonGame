9e6eb5e9e27c99ff143ef24b74be82f047accb62
ilandroxxy <ilyand98@mail.ru>
2019-04-28 16:59:35 +0700

    Added keyPress

--- src/main.cpp
+++ src/main.cpp
@@ -61,6 +61,15 @@ int main()
                 window.close();
             }
 
+            if (Keyboard::isKeyPressed(Keyboard::Left))
+                dir = 1;
+            if (Keyboard::isKeyPressed(Keyboard::Right))
+                dir = 2;
+            if (Keyboard::isKeyPressed(Keyboard::Up))
+                dir = 3;
+            if (Keyboard::isKeyPressed(Keyboard::Down))
+                dir = 0;
+
             if (timer > delay) {
                 timer = 0;
                 Tick();
