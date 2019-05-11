043575c335a29b19552732bad9e022d18c64b91d
AlexeyDer <hottelmich@gmail.com>
2019-05-11 17:20:28 +0700

    Add func for convert from int to string

--- src/binding.h
+++ src/binding.h
@@ -3,6 +3,7 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 
@@ -16,5 +17,18 @@ Tick(int& num,
      int& iter);
 double Lev(int& num, int& max1, int& max2, int& spe, int& dir, float& delay);
 double Lev2(int& num, int& max1, int& max2, int& spe, int& dir, float& delay);
+
+template <typename T>
+std::string To_string(T value)
+{
+    // create an output string stream
+    std::ostringstream os;
+
+    // throw the value into the string stream
+    os << value;
+
+    // convert the string stream into a string and return
+    return os.str();
+}
 
 #endif // BINDING_H
