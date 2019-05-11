cd55736ae52a4c8b9ff12cb51ceec5478f9c24b7
AlexeyDer <hottelmich@gmail.com>
2019-05-11 17:48:11 +0700

    Add SuperFrute for lev 2

--- src/lev2.cpp
+++ src/lev2.cpp
@@ -1,3 +1,4 @@
+#include "SuperFrute.hpp"
 #include "binding.h"
 #include "fruct.h"
 #include "level2.h"
@@ -5,7 +6,14 @@
 
 using namespace std;
 
-double Lev2(int& num, int& max1, int& max2, int& spe, int& dir, float& delay)
+double
+Lev2(int& num,
+     int& max1,
+     int& max2,
+     int& spe,
+     int& dir,
+     float& delay,
+     int& iter)
 {
     int N = 30, M = 20;
 
@@ -45,6 +53,12 @@ double Lev2(int& num, int& max1, int& max2, int& spe, int& dir, float& delay)
 
         f.x = rand() % N;
         f.y = rand() % M;
+
+        iter++;
+        if (iter != 1 && (iter % 5 == 0)) {
+            suf.x = rand() % N;
+            suf.y = rand() % M;
+        }
     }
 
     if ((s[0].x == t.x) && (s[0].y == t.y)) {
@@ -405,6 +419,28 @@ double Lev2(int& num, int& max1, int& max2, int& spe, int& dir, float& delay)
 
         t9.x = rand() % N;
         t9.y = rand() % M;
+    }
+
+    if ((s[0].x == suf.x) && (s[0].y == suf.y)) {
+        num += 4;
+        if (num >= max1)
+            max1 = num;
+        if (num >= max2)
+            max2 = num;
+        if (num > 5) {
+            delay = 0.09;
+            spe = 2;
+        }
+        if (num > 10) {
+            delay = 0.06;
+            spe = 3;
+        }
+        if (num > 15) {
+            delay = 0.04;
+            spe = 4;
+        }
+        suf.x = -1;
+        suf.y = -1;
     }
 
     if (s[0].x > N - 1)
