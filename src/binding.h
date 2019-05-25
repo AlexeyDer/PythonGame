351621f9e708fc8b545bfb9a99904ff4b475e377
AlexeyDer <hottelmich@gmail.com>
2019-05-25 13:30:38 +0700

    Add structs from .h files

--- src/binding.h
+++ src/binding.h
@@ -7,6 +7,53 @@
 
 using namespace std;
 
+struct Fruct {
+    int x;
+    int y;
+};
+extern Fruct f;
+
+struct Level1 {
+    int x;
+    int y;
+};
+
+extern Level1 p;
+extern Level1 p1;
+extern Level1 p2;
+extern Level1 p3;
+extern Level1 p4;
+
+struct Level2 {
+    int x;
+    int y;
+};
+
+extern Level2 t;
+extern Level2 t1;
+extern Level2 t2;
+extern Level2 t3;
+extern Level2 t4;
+extern Level2 t5;
+extern Level2 t6;
+extern Level2 t7;
+extern Level2 t8;
+extern Level2 t9;
+
+struct Snake {
+    int x;
+    int y;
+};
+
+extern Snake s[100];
+
+struct Sfruct {
+    int x;
+    int y;
+};
+
+extern Sfruct suf;
+
 double
 Tick(int& num,
      int& max1,
