c06db3917235f21370602142744e6b4402f34dea
AlexeyDer <hottelmich@gmail.com>
2019-05-25 15:42:27 +0700

    Rewrite condition press button on separate functions

--- src/main.cpp
+++ src/main.cpp
@@ -133,31 +133,11 @@ int main()
             }
         }
 
-        if (Keyboard::isKeyPressed(Keyboard::Left) && R == 1) {
-            dir = 1;
-            L = 0;
-            U = 1;
-            D = 1;
-        }
+        KeyBoardPressLeft(Keyboard::Left, R, dir, L, U, D);
+        KeyBoardPressRight(Keyboard::Right, R, dir, L, U, D);
+        KeyBoardPressUp(Keyboard::Up, R, dir, L, U, D);
+        KeyBoardPressDown(Keyboard::Down, R, dir, L, U, D);
 
-        if (Keyboard::isKeyPressed(Keyboard::Right) && L == 1) {
-            dir = 2;
-            R = 0;
-            U = 1;
-            D = 1;
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Up) && D == 1) {
-            dir = 3;
-            U = 0;
-            L = 1;
-            R = 1;
-        }
-        if (Keyboard::isKeyPressed(Keyboard::Down) && U == 1) {
-            dir = 0;
-            D = 0;
-            L = 1;
-            R = 1;
-        }
         if (Keyboard::isKeyPressed(Keyboard::Q))
             return 0;
 
