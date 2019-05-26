d0d19283857c344c9b8c477b7bca3da8434205c6
AlexeyDer <hottelmich@gmail.com>
2019-05-26 16:13:07 +0700

    Add folder thirdparty for .h and .clang

---
+++ thirdparty/MyTests.h
@@ -0,0 +1,14 @@
+
+// KeyBoardPressLeft((KeyBoard::Left, 1, 1, 1 ,1, 1)
+
+// class MyTestSuite : public CxxTest::TestSuite {
+// public:
+//     void testAddition(void)
+//     {
+//         TS_ASSERT_EQUALS(KeyBoardPressLeft((KeyBoard::Left, 1, 1, 1 ,1, 1),
+//         0); TS_ASSERT_EQUALS(KeyBoardPressLeft((KeyBoard::Left, 0, 1, 1 ,1,
+//         1), 1); TS_ASSERT_EQUALS(KeyBoardPressLeft((KeyBoard::Right, 1, 1, 1
+//         ,1, 1), 1); TS_ASSERT_EQUALS(KeyBoardPressLeft((KeyBoard::Up, 1, 1, 1
+//         ,1, 1), 1);
+//     }
+// };
