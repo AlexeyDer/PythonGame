5715a3028a3b43c2e2046be25a2c81e671e2d518
AlexeyDer <hottelmich@gmail.com>
2019-05-27 01:33:53 +0700

    Modify functions KeyBoardPress

--- src/binding.cpp
+++ src/binding.cpp
@@ -18,11 +18,13 @@ int KeyBoardPressLeft(
 int KeyBoardPressUp(
         Keyboard::Key button, int& R, int& dir, int& L, int& U, int& D)
 {
-    if (Keyboard::isKeyPressed(button) && D == 1) {
-        dir = 3;
-        U = 0;
-        L = 1;
-        R = 1;
+    if ((button == Keyboard::Up) && D == 1) {
+        if (Keyboard::isKeyPressed(button)) {
+            dir = 3;
+            U = 0;
+            L = 1;
+            R = 1;
+        }
         return 0;
     }
     return 1;
@@ -31,11 +33,13 @@ int KeyBoardPressUp(
 int KeyBoardPressRight(
         Keyboard::Key button, int& R, int& dir, int& L, int& U, int& D)
 {
-    if (Keyboard::isKeyPressed(button) && L == 1) {
-        dir = 2;
-        R = 0;
-        U = 1;
-        D = 1;
+    if ((button == Keyboard::Right) && (L == 1)) {
+        if (Keyboard::isKeyPressed(button)) {
+            dir = 2;
+            R = 0;
+            U = 1;
+            D = 1;
+        }
         return 0;
     }
     return 1;
@@ -44,11 +48,13 @@ int KeyBoardPressRight(
 int KeyBoardPressDown(
         Keyboard::Key button, int& R, int& dir, int& L, int& U, int& D)
 {
-    if (Keyboard::isKeyPressed(button) && U == 1) {
-        dir = 0;
-        D = 0;
-        L = 1;
-        R = 1;
+    if ((button == Keyboard::Down) && U == 1) {
+        if (Keyboard::isKeyPressed(button)) {
+            dir = 0;
+            D = 0;
+            L = 1;
+            R = 1;
+        }
         return 0;
     }
     return 1;
