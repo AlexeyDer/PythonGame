26fd832a22e093f919e960e62250ce916a02708c
AlexeyDer <hottelmich@gmail.com>
2019-05-27 01:04:18 +0700

    Add tests for KeyBoardPressLeft func

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,31 @@
 #include "../src/binding.h"
 #include <gtest/gtest.h>
 
-TEST(MovingPuzzle, move_1)
+TEST(KeyBoardPress, Left_1)
 {
-    ASSERT_EQ(2, 2);
+    int a, b, c, d, e;
+    a = 1;
+    b = c = e = d = 1;
+    int res = KeyBoardPressLeft(Keyboard::Left, a, b, c, d, e);
+    ASSERT_EQ(res, 0);
+}
+
+TEST(KeyBoardPress, Left_2)
+{
+    int a, b, c, d, e;
+    a = 1;
+    b = c = e = d = 1;
+    int res = KeyBoardPressLeft(Keyboard::Right, a, b, c, d, e);
+    ASSERT_EQ(res, 1);
+}
+
+TEST(KeyBoardPress, Left_3)
+{
+    int a, b, c, d, e;
+    a = 0;
+    b = c = e = d = 1;
+    int res = KeyBoardPressLeft(Keyboard::Left, a, b, c, d, e);
+    ASSERT_EQ(res, 1);
 }
 
 int main(int argc, char** argv)
