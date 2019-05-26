aa13a204d2c912f80edb22a48e2d21b2419b07fb
AlexeyDer <hottelmich@gmail.com>
2019-05-26 16:12:27 +0700

    Add cpp test file

--- test/test.cpp
+++ test/test.cpp
@@ -1,22 +1,13 @@
-#ifndef CXXTEST_RUNNING
-#define CXXTEST_RUNNING
-#endif
+#include "../src/binding.h"
+#include <gtest/gtest.h>
 
-#define _CXXTEST_HAVE_STD
-#include "MyTests.h"
-#include <cxxtest/ErrorPrinter.h>
-#include <cxxtest/RealDescriptions.h>
-#include <cxxtest/TestListener.h>
-#include <cxxtest/TestMain.h>
-#include <cxxtest/TestRunner.h>
-#include <cxxtest/TestTracker.h>
-
-int main(int argc, char* argv[])
+TEST(MovingPuzzle, move_1)
 {
-    int status;
-    CxxTest::ErrorPrinter tmp;
-    CxxTest::RealWorldDescription::_worldName = "cxxtest";
-    status = CxxTest::Main<CxxTest::ErrorPrinter>(tmp, argc, argv);
-    return status;
+    ASSERT_EQ(2, 2);
 }
-bool suite_MyTest_init = false;
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
