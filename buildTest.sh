aa83b507479bec052a78993e160db4060b83c39a
AlexeyDer <hottelmich@gmail.com>
2019-05-30 23:39:37 +0700

    Delete download in buildTest

--- buildTest.sh
+++ buildTest.sh
@@ -7,15 +7,3 @@ sudo cmake CMakeLists.txt
 sudo make
 sudo cp *.a /usr/lib
 cd -
-
-mkdir lib
-wget https://github.com/google/googletest/archive/release-1.8.1.tar.gz
-tar xf release-1.8.1.tar.gz
-rm -rf release-1.8.1.tar.gz
-cd googletest-release-1.8.1
-cmake -DBUILD_SHARED_LIBS=ON .
-make
-cd -
-mv googletest-release-1.8.1/googletest/include/ include/
-mv googletest-release-1.8.1/googlemock/gtest/lib*.so lib/
-rm -rf googletest-release-1.8.1
